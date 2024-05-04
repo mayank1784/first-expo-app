92a9760a639e453e1a5ca1a6900496caa807fbed
Mayank Jain <mayankjain1784@gmail.com>
2024-05-04 18:48:42 +0530

    auth logic with appwrite done
    appwrite connected, datav\base made, global user context applied

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,19 +1,40 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Alert, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from "../../constants"
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
+import { getCurrentUser, signIn } from '../../lib/appwrite'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
 const SignIn = () => {
+  const {setUser, setIsLoggedIn} = useGlobalContext()
   const [form, setForm] = useState({
     email: '',
     password: ''
   })
 
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const submit = ()=>{
+  const submit = async()=>{
+    if (form.email==="" || form.password===''){
+      Alert.alert('Error', "Please fill in all the fields")
+    }
+    setIsSubmitting(true)
+    try {
+      await signIn(form.email, form.password)
+      const result = await getCurrentUser()
+      setUser(result)
+      setIsLoggedIn(true)
+
+      Alert.alert("Success","User signed in successfully")
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error',error.message)
+    }finally{
+      setIsSubmitting(false)
+    }
+  
 
   }
 
---
+++ context/GlobalProvider.js
@@ -0,0 +1,43 @@
+import {createContext, useContext, useEffect, useState} from 'react'
+import { getCurrentUser } from '../lib/appwrite'
+const GlobalContext = createContext()
+export const useGlobalContext = () => {
+    return useContext(GlobalContext)
+}
+
+const GlobalProvider = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=>{
+        getCurrentUser().then((res)=>{
+            if(res){
+            setIsLoggedIn(true)
+            setUser(res)
+    } else{
+        setIsLoggedIn(false)
+        setUser(null)
+    }
+}).catch((e)=> console.log(e)).finally(()=>{
+    setIsLoading(false)
+
+})
+    },[])
+
+    return(
+        <GlobalContext.Provider
+        value={{
+            isLoggedIn,
+            setIsLoggedIn,
+            user,
+            setUser,
+            isLoading
+        }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export default GlobalProvider
