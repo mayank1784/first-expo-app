64b8af39e8ebdabe3762aee15cc47b3bb95767ef
Mayank Jain <mayankjain1784@gmail.com>
2024-05-06 19:15:31 +0530

    added trending video section and video playing

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,20 +1,27 @@
-import { TouchableOpacity, Text } from 'react-native'
-import React from 'react'
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
 
-const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
   return (
-    <TouchableOpacity 
-    onPress={handlePress}
-    activeOpacity={0.7}
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
-    className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
-    disabled={isLoading}> 
-        <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
-            {title}
-        </Text>
-        </TouchableOpacity>
-  )
-}
-
-export default CustomButton
-
+export default CustomButton;
